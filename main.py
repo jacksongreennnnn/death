26a7e4435b3acd520799ece3d5f559d8faa5f35d
BaguetteYeeter <baguetteyeeter@gmail.com>
2021-11-16 15:59:33 +0000

    End of tuesday 16.11.21

--- main.py
+++ main.py
@@ -2,25 +2,34 @@ numberOfCopies = 20
 price = 9.99
 name = "DOOM"
 profit = 0
+copiesSold = 0
+sold5Copies = False
+customers = []
 
-def sellACopy(copies):
-  global numberOfCopies, profit
+def sellACopy(copies, customer="bob"):
+  global numberOfCopies, profit, copiesSold, customers
   numberOfCopies -= copies
   profit += (price * copies)
+  copiesSold += 1
+  customers.append(customer)
 
 def checkCopies():
   if numberOfCopies <= 0:
     print("NOOOOOOOOOOOOOOOOOO")
   else:
     print("We have %d copies of doom" % numberOfCopies)
-    print("Our profit is £%r" % profit) 
+    print("Our profit is £%.2f" % profit)
+  if copiesSold >= 5:
+    sold5Copies = True
+    print("We sold more than 5 copies")
   
 numberOfCopies -= 1
 profit += price
 
 checkCopies()
 
-for i in range(0, 19):
+for i in range(0, 5):
   sellACopy(1)
 
 checkCopies()
+print(customers)
---
+++ tmp.py
@@ -0,0 +1,10 @@
+a = 5
+b = 3
+
+def add():
+  global a, b
+  a += 1
+  b -= 1
+
+add()
+print(a, b)
